3a05a230f13aa2a83b6cf6d80d9719836ca980f3
sim7457 <118698648+sim7457@users.noreply.github.com>
2023-02-13 17:44:59 +0900

    230213

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
 const TOP_BANNER = document.querySelector('#header .topBanner');
 const TOP_BANNER_BTN = document.querySelector('#header i');
 
+const MAIN_SLIDE_NAV = document.querySelector('#mainVisual .slide_nav');
+const MAIN_SLIDE_NAV_LI = document.querySelectorAll('#mainVisual .slide_nav>li');
+
+const MAIN_SLIDE_NUM = document.querySelector('#mainVisual .num');
+
 const TOP_BANNER_HANDLER = () => {
     TOP_BANNER.classList.add('on');
 }
@@ -25,14 +30,146 @@ const HD_WRAP = document.querySelector('#header .hdWap');
 
 const HD_WRAP_HANDLER = () => {
     let SCT = window.scrollY;
-    //console.log('스크롤량 : ', SCT);
-    //HD_WRAP.classList.toggle('on');
     SCT > 0
         ? HD_WRAP.classList.add('on')
         : HD_WRAP.classList.remove('on');
 }
 
-window.addEventListener('scroll', HD_WRAP_HANDLER)
+window.addEventListener('scroll', HD_WRAP_HANDLER);
+
+const MAIN_VISUAL_SLIDE_OPTION = {
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    // navigation: {
+    //     nextEl: "#mainVisual .arrows>div:nth-child(1)",
+    //     prevEl: "#mainVisual .arrows>div:nth-child(2)",
+    // },
+    on: {
+        slideChangeTransitionStart: function () {
+            console.log(this, this.realIndex, MAIN_SLIDE_NAV_LI);
+            let idx = this.realIndex;
+            let total = this.slides.length;
+            MAIN_SLIDE_NAV_LI.forEach(it => it.classList.remove('on'));
+            MAIN_SLIDE_NAV_LI[idx].classList.add('on');
+            MAIN_SLIDE_NUM.innerHTML = `<strong>${idx < 9 ? 0 : ''}${idx + 1}</strong> / <span>${total - 2}</span>`;
+        }
+    }
+}
+
+const MAIN_VISUAL_SLIDE = new Swiper('.mainSlide', MAIN_VISUAL_SLIDE_OPTION);
+
+const MAIN_VISUAL_SLIDE_ARROWS = document.querySelectorAll('#mainVisual .arrows>div');
+console.log(MAIN_VISUAL_SLIDE_ARROWS[0]);
+
+MAIN_VISUAL_SLIDE_ARROWS[0].addEventListener('click', () => {
+    MAIN_VISUAL_SLIDE.slidePrev();
+});
+MAIN_VISUAL_SLIDE_ARROWS[1].addEventListener('click', () => {
+    MAIN_VISUAL_SLIDE.slideNext();
+});
+
+//이벤트의 위임... e.target --> 
+
+const MAIN_SLIDE_NAV_HANDLER = e => {
+    e.preventDefault();
+    //console.log(e.target);
+    // for (let i = 0; i < MAIN_SLIDE_NAV_LI.length; i++) {
+    //     MAIN_SLIDE_NAV_LI[i].classList.remove('on')
+    // }
+    const TG = e.target.parentElement;
+    //console.log(MAIN_SLIDE_NAV_LI, [...MAIN_SLIDE_NAV_LI]);
+    const I = [...MAIN_SLIDE_NAV_LI].indexOf(TG);
+    //console.log(I)
+    //$(this).parent().index();
+    //TG.classList.add('on');
+    MAIN_VISUAL_SLIDE.slideTo(I + 1);
+}
+
+
+MAIN_SLIDE_NAV.addEventListener('click', MAIN_SLIDE_NAV_HANDLER);
+
+const PF_LEFT_SLIDE_OPTION = {
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    initialSlide: 1,
+    navigation: {
+        nextEl: "#mainPortfolio .arrows li:nth-child(2)",
+        prevEl: "#mainPortfolio .arrows li:nth-child(1)",
+    },
+}
+const PF_LEFT_SLIDE = new Swiper('.pf_left_slide', PF_LEFT_SLIDE_OPTION);
+
+const PF_RIGHT_SLIDE_OPTION = {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 20,
+    touchRatio: 0.2,
+    initialSlide: 1,
+    slideToClickedSlide: true, // 클릭한 슬라이드로 이동
+    loopAdditionalSlides: 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+}
+const PF_RIGHT_SLIDE = new Swiper('.pf_right_slide', PF_RIGHT_SLIDE_OPTION);
+
+PF_LEFT_SLIDE.controller.control = PF_RIGHT_SLIDE;
+PF_RIGHT_SLIDE.controller.control = PF_LEFT_SLIDE;
+
+//https://shadesign.tistory.com/entry/swiper-slide-%EC%B4%9D%EC%A0%95%EB%A6%AC%EC%82%AC%EC%9A%A9%EB%B2%95-%EC%A0%81%EC%9A%A9-%EC%98%B5%EC%85%98
+//https://www.biew.co.kr/entry/Swiper-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-Swiper-2%EA%B0%9C-%EC%97%B0%EB%8F%99%EA%B3%BC-%EC%A0%9C%EC%96%B4Controller
+
+const MS_CONTENT = document.querySelectorAll('#mainSolution .Ms_content .content');
+const MS_NUM = document.querySelector('#mainSolution .num');
+
+const MS_SLIDE_OPTION = {
+    loop: true,
+    spaceBetween: 100,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    slideActiveClass: "on",
+    on: {
+        slideChangeTransitionStart: function () {
+            console.log(this, this.realIndex, this.slides.length);
+            let idx = this.realIndex;
+            let total = MS_CONTENT.length;
+            // for (let i = 0; i < MS_CONTENT.length; i++) {
+            //     MS_CONTENT[i].classList.remove('on');
+            // }
+
+            MS_NUM.innerHTML = `<strong>${idx < 10 ? '0' : ''}${idx + 1}</strong> / <span>${total < 10 ? '0' : ''}${total}</span>`;
+
+            MS_CONTENT.forEach(it => it.classList.remove('on'));
+            MS_CONTENT[idx].classList.add('on');
+        }
+    },
+
+    navigation: {
+        nextEl: "#mainSolution .arrows li:nth-child(2)",
+        prevEl: "#mainSolution .arrows li:nth-child(1)",
+    },
+
+    pagination: {
+        el: '#mainSolution .dots',
+        clickable: true,
+    },
+
+
+
+}
+const MS_SLIDE = new Swiper('.Ms_slide', MS_SLIDE_OPTION);
+
+
+
+
+
+
+
+
+
 
 
 
